135dc9b41c9785bbfce664b8f1cb7af0afbd2e23
theedigerati <39467790+theedigerati@users.noreply.github.com>
2024-03-16 05:33:18 +0100

    add tax model & migration

--- apps/tax/models.py
+++ apps/tax/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Tax(models.Model):
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+    name = models.CharField(max_length=100, unique=True)
+    number = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        verbose_name_plural = _("Taxes")
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.rate}%)"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "rate": self.rate,
+            "name": self.name,
+            "number": self.number,
+        }
