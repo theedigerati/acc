04ee43534a4b5d4dd03374975a99a0b999daff65
theedigerati <39467790+theedigerati@users.noreply.github.com>
2024-03-19 06:23:12 +0100

    add client app model & migration

--- apps/sales/client/apps.py
+++ apps/sales/client/apps.py
@@ -1,8 +1,9 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ClientConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     label = "client"
     name = "apps.sales.client"
-    verbose_name = "Client"
+    verbose_name = _("Client")
