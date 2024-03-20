285465780e9610e1f292d85771a47293a02c0c91
theedigerati <39467790+theedigerati@users.noreply.github.com>
2024-03-20 17:57:38 +0100

    add vendor model & migrations

--- apps/purchase/vendor/models.py
+++ apps/purchase/vendor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Vendor(models.Model):
+    full_name = models.CharField(max_length=100, blank=True)
+    business_name = models.CharField(max_length=100, blank=True)
+    display_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, blank=True)
+    phone = models.CharField(max_length=16, blank=True)
+    shipping_address = models.ForeignKey(
+        "address.Address", null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    def __str__(self) -> str:
+        return self.display_name
